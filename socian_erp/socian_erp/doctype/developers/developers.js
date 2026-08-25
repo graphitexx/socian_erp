51bb16a4067af5e2169f289186098ef725a4cd6a
Pavel Kosberg <pavelkosberg@gmail.com>
2026-08-25 17:01:44 +0500

    Add new doctypes and updates

---
+++ socian_erp/socian_erp/doctype/developers/developers.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, й and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Developers", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ socian_erp/socian_erp/doctype/residential_complex/residential_complex.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, й and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("residential complex", {
+// 	refresh(frm) {
+
+// 	},
+// });
